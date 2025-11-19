d961c5fb0ea5f4fa0349bd9506b61efc5086c645
tanapon010 <664230010@webmail.npru.ac.th>
2025-11-19 15:45:25 +0700

    Update Home.py

--- Home.py
+++ Home.py
@@ -49,3 +49,38 @@ if st.button("แสดงการจินตทัศน์ข้อมูล
    st.button("ไม่แสดงข้อมูล")
 else:
     st.write("ไม่แสดงข้อมูล")
+
+html_8 = """
+<div style="background-color:#6BD5DA;padding:15px;border-radius:15px 15px 15px 15px;border-style:'solid';border-color:black">
+<center><h5>ทำนายข้อมูล</h5></center>
+</div>
+"""
+st.markdown(html_8, unsafe_allow_html=True)
+st.markdown("")
+
+pt_len = st.slider("กรุณาเลือกข้อมูล petallength")
+pt_wd = st.slider("กรุณาเลือกข้อมูล petalwidth")
+
+sp_len = st.number_input("กรุณาเลือกข้อมูล sepallength")
+sp_wd = st.number_input("กรุณาเลือกข้อมูล sepalwidth")
+
+if st.button("ทำนายผล"):
+    #st.write("ทำนาย")
+   dt = pd.read_csv("./data/iris.csv") 
+   X = dt.drop('variety', axis=1)
+   y = dt.variety   
+   Knn_model = KNeighborsClassifier(n_neighbors=3)
+   Knn_model.fit(X, y)   
+   x_input = np.array([[pt_len, pt_wd, sp_len, sp_wd]])
+   st.write(Knn_model.predict(x_input))
+   
+   out=Knn_model.predict(x_input)
+
+   if out[0] == 'Setosa':
+    st.image("./img/iris1.jpg")
+   elif out[0] == 'Versicolor':       
+    st.image("./img/iris2.jpg")
+   else:
+    st.image("./img/iris3.jpg")
+else:
+    st.write("ไม่ทำนาย")

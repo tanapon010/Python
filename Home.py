3f9845e345a164d0551e81f4433b31e99c6a1c1b
tanapon010 <664230010@webmail.npru.ac.th>
2025-11-19 15:48:42 +0700

    Update Home.py

--- Home.py
+++ Home.py
@@ -51,7 +51,7 @@ else:
     st.write("ไม่แสดงข้อมูล")
 
 html_8 = """
-<div style="background-color:#6BD5DA;padding:15px;border-radius:15px 15px 15px 15px;border-style:'solid';border-color:black">
+<div style="background-color:#FF69B4;padding:15px;border-radius:15px 15px 15px 15px;border-style:'solid';border-color:black">
 <center><h5>ทำนายข้อมูล</h5></center>
 </div>
 """

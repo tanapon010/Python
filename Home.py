2b00b7e0f46c681a50d2e534c2ac215fe9069b19
tanapon010 <664230010@webmail.npru.ac.th>
2025-11-19 15:27:08 +0700

    Update Home.py

---
+++ Home.py
@@ -0,0 +1,9 @@
+from sklearn.neighbors import KNeighborsClassifier
+import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+st.header('Tanapon')
+st.image("./img/Tanapon.jpg")
